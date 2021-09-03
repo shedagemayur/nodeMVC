676951a0a976f959ae42af16fcabab7efac58541
mayurshedage <mayur.shedage@inscripts.in>
2021-09-03 12:34:04 +0530

    init authors

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout');
+
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
@@ -21,8 +29,8 @@ const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to mongoose'))
 
-const indexRoute = require('./routes/index');
-app.use(indexRoute);
+app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 
 app.listen(process.env.PORT || 3000);
