3ed3a2613dc7f7c85b69cf4c5e6811c0fc519519
mayurshedage <mayur.shedage@inscripts.in>
2021-09-04 20:37:39 +0530

    all set

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,12 +1,26 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode
-);
+const rootStyle = window.getComputedStyle(document.documentElement);
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150/100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-});
-FilePond.parse(document.body);
+if(rootStyle.getPropertyValue('--book-cover-width-large') != null && rootStyle.getPropertyValue('--book-cover-width-large') != '') {
+    ready();
+} else {
+    document.getElementById('main-css').addEventListener('load', ready);
+}
+
+function ready() {
+    const coverWidth = parseFloat(rootStyle.getPropertyValue('--book-cover-width-large'));
+    const aspectRatio = parseFloat(rootStyle.getPropertyValue('--book-cover-aspect-ratio'));
+    const coverHeight = parseFloat(coverWidth / aspectRatio);
+
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode
+    );
+
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / aspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    });
+    FilePond.parse(document.body);
+}
